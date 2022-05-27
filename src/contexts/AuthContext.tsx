09ccbde1a8b35f919abc83c211cc303e7fabb306
Giulia Fiorenzano <giulia.fiorenzano@alibin.com.br>
2022-05-27 17:00:14 -0300

    feat: add dados nos cookies

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import Router from "next/router";
+import { setCookie, parseCookies } from "nookies";
 import { api } from "../services/api";
 
 type User = {
@@ -29,6 +30,16 @@ export function AuthProvider({ children }: AuthProviderProps) {
     const [ user, setUser ] = useState<User>();
     const isAuthenticated = !!user;
 
+    useEffect(() => {
+        const { "nextauth.token": token} = parseCookies()
+        if(token) {
+            api.get("/me").then(response => {
+               const { email, permissions, roles} = response.data
+               setUser({ email, permissions, roles})
+            })
+        }
+    }, [])
+
     async function signIn({email, password}: SignInCredentials) {
         try {
             const response = await api.post("sessions", {
@@ -37,12 +48,25 @@ export function AuthProvider({ children }: AuthProviderProps) {
             })
 
             const { token, refreshToken, permissions, roles } = response.data
-    
+            
+            //primeiro parametro é undefined porque no caso de agr tá tudo acontecendo pelo browser (e nao pelo servidor)
+            //segundo é o nome do que vc quer armazenar, terceiro o dado q vai armazenar
+            //maxAge é qnto tempo pra expirar
+            //path: "/" qlquer lugar da aplicação tem acesso ao cookie
+            setCookie(undefined, "nextauth.token", token, {
+                maxAge: 60 * 60 * 24 * 30, //30 dias
+                path: "/"
+            })
+
+            setCookie(undefined, "nextauth.refreshToken", refreshToken)
+
             setUser({
                 email,
                 permissions,
                 roles
             })
+
+            api.defaults.headers.common["Authorization"] = `Bearer ${token}`
 
             Router.push("/dashboard")
 
