08c63cd5a8b56266eb5ee1dc535450a17bef2789
Giulia Fiorenzano <giulia.fiorenzano@alibin.com.br>
2022-05-30 13:04:31 -0300

    feat: validando visitante

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
+import { GetServerSideProps } from "next";
 import { FormEvent, useContext, useState } from "react";
+import { parseCookies } from "nookies";
 import { AuthContext } from "../contexts/AuthContext";
 import styles from "../styles/Home.module.css";
+import { withSSRGuest } from "../utils/withSSRGuest";
 
 const Home = () => {
   const [ email, setEmail ] = useState('');
@@ -30,3 +33,11 @@ const Home = () => {
 }
 
 export default Home
+
+export const getServerSideProps = withSSRGuest(async (ctx) => {
+  return {
+    props: {
+      
+    }
+  }
+})
