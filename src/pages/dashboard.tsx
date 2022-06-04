da477131aec0fc96d2d3abfd69b4bc95a2d3a8cb
giufiorenzano <giufiorenzano@gmail.com>
2022-06-03 21:36:39 -0300

    feat: controle de permissions e roles

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,8 @@
 import { GetServerSideProps } from "next"
 import { useContext, useEffect } from "react"
+import { Can } from "../components/Can"
 import { AuthContext } from "../contexts/AuthContext"
+import { useCan } from "../hooks/useCan"
 import { setupAPIClient } from "../services/api"
 import { api } from "../services/apiClient"
 import { withSSRAuth } from "../utils/withSSRAuth"
@@ -8,13 +10,25 @@ import { withSSRAuth } from "../utils/withSSRAuth"
 export default function Dashboard() {
     const { user, isAuthenticated} = useContext(AuthContext)
 
+    const userCanseeMetris = useCan({
+        permissions: ["metrics.list"]
+    })
+
     useEffect(() => {
         api.get("/me")
         .then(response => {console.log(response)})
         .catch(err => { console.log(err)})
     })
     return (
+        <>
         <h1>Dashboard: {user?.email}</h1>
+
+        <Can permissions={["metrics.list"]}>
+          <div>Métricas</div>
+        </Can>
+        
+        </>
+        
     )
 }
 
---
+++ src/pages/metrics.tsx
@@ -0,0 +1,26 @@
+import { GetServerSideProps } from "next"
+import { setupAPIClient } from "../services/api"
+import { withSSRAuth } from "../utils/withSSRAuth"
+
+export default function Metrics() {
+    
+    return (
+        <>
+        <h1>Metrics</h1>
+        
+        </>
+        
+    )
+}
+
+export const getServerSideprops: GetServerSideProps = withSSRAuth(async (ctx) => {
+    const apiClient = setupAPIClient(ctx)
+    const response = await apiClient.get("/me")
+
+    return {
+        props: {}
+    }
+}, {
+    permissions: ["metrics.list"],
+    roles: ["administrator"],
+})
